503398ddbb2a05c397031056e0b63067ce15bd7e
rkfr <e.xbyvnqn@gmail.com>
2019-12-02 18:03:42 +0200

    the "Airport Arrivals/Departures - #1" task was solved

---
+++ airport-arrivals-departures-1/README.md
@@ -0,0 +1,1 @@
+[Airport Arrivals/Departures - #1](https://www.codewars.com/kata/57feb00f08d102352400026e/)
---
+++ airport-arrivals-departures-1/airportArrivalsDepartures.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const rotor = {
+	rotorLine: "ABCDEFGHIJKLMNOPQRSTUVWXYZ ?!@#&()|<>.:=-+*/0123456789",
+
+	getIndex(char) {
+		return this.rotorLine.indexOf(char);
+	},
+
+	getFlappedChar(char, flaps = 0) {
+		const charNum = this.getIndex(char);
+		const rotorLength = this.rotorLine.length;
+
+		let flappedChar = "";
+		let steps = charNum;
+		for (let i = 0; i <= flaps; i++) {
+			if (steps >= rotorLength) {
+				steps = 0;
+			}
+			flappedChar = this.rotorLine[steps];
+			steps++;
+		}
+
+		return flappedChar;
+	},
+
+	flapLine(line, flaps) {
+		return line.split("").map(char => rotor.getFlappedChar(char, flaps));
+	}
+};
+
+const flapDisplay = function(lines, rotors) {
+	return rotors.map((rotorItem, i) => {
+		const line = lines[i];
+		let flapped = [];
+		let cachedFlaps = 0;
+
+		rotorItem.forEach((flaps, j) => {
+			cachedFlaps += flaps;
+			flapped.push(...rotor.flapLine(line, cachedFlaps).slice(j, j + 1));
+		});
+
+		return flapped.join("");
+	});
+};
